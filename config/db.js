0e854d7397574a34ab9cfafe8fd2786342008e2d
Sai-sree1 <131637155+Sai-sree1@users.noreply.github.com>
2024-11-18 16:35:46 +0530

    Delete config directory

--- config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-exports.connectDB = async (MONGO_URI) => {
-	return mongoose.connect(MONGO_URI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useCreateIndex: true,
-	});
-};
